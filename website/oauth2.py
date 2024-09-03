0d1ad61fd717f24444f84fd23edc9db39dfa9389
aligomaa56 <3looosh56565656@gmail.com>
2024-09-03 22:04:05 +0300

    CRT: Create oauth2 file and handel jwt generate func

---
+++ website/oauth2.py
@@ -0,0 +1,42 @@
+from datetime import datetime, timedelta
+from database import get_db
+from models import User
+import jwt
+from config import settings
+from schema import TokenData
+
+
+SECRET_KEY=settings.secret_key
+ALGORITHM=settings.algorithm
+ACCESS_TOKEN_EXPIRE_MINUTES=settings.access_token_expire_minutes
+
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+def verify_access_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_id = payload.get("user_id")
+        user_name = payload.get("user_name")
+        if not user_id or not user_name:
+            return None
+        token_data = TokenData(user_id=user_id, user_name=user_name)
+        return token_data
+    except jwt.ExpiredSignatureError:
+        return None
+    except jwt.InvalidTokenError:
+        return None
+
+def get_current_user(token: str, db=get_db):
+    token_data = verify_access_token(token)
+    if not token_data:
+        return None
+    user = db.query(User).filter(User.id == token_data.user_id, User.username == token_data.user_name).first()
+    if not user:
+        return None
+    return user
