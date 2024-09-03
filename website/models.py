6a21eec3a8edd0b070c38744f89c090186a9dcb1
aligomaa56 <3looosh56565656@gmail.com>
2024-09-03 22:00:30 +0300

    EDT: make changes in user model (adding avatar col, change hashed_password size)

--- website/models.py
+++ website/models.py
@@ -11,8 +11,10 @@ class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True, nullable=False)
     username = Column(String(50), unique=True, index=True, nullable=False)
-    email = Column(String(50), unique=True, index=True, nullable=False)
-    hashed_password = Column(String(50), nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
+    first_name = Column(String, nullable=False, index=True)
+    last_name = Column(String, nullable=False, index=True)
+    hashed_password = Column(String, nullable=False)
     role = Column(String(50), nullable=False)
     is_blocked = Column(Boolean, default=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
