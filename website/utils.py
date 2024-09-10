d1a3c279ef9ef85fa45bb53c38edb329a5fed102
aligomaa56 <3looosh56565656@gmail.com>
2024-09-10 04:58:43 +0300

    ADD: adding evaluate_quiz func that calc the score

---
+++ website/utils.py
@@ -0,0 +1,29 @@
+from models import Question, QuizAttempt, CorrectAnswer, Question
+from database import get_db
+
+
+def evaluate_quiz(quiz_attempt_id):
+    db = get_db()
+    quiz_attempt = db.query(QuizAttempt).filter(QuizAttempt.id == quiz_attempt_id).first()
+    
+    total_score = 0
+    for answer in quiz_attempt.answers:
+        question = db.query(Question).filter(Question.id == answer.question_id).first()
+        correct_answer = db.query(CorrectAnswer).filter(CorrectAnswer.question_id == question.id).first()
+        
+        if question.question_type == 'true_false':
+            if answer.content == correct_answer.correct_answer:
+                answer.points_awarded = question.points
+                total_score += question.points
+        elif question.question_type == 'choose':
+            if answer.content == correct_answer.correct_answer:
+                answer.points_awarded = question.points
+                total_score += question.points
+        elif question.question_type == 'written':
+            if answer.content == correct_answer.correct_answer:
+                answer.points_awarded = question.points
+                total_score += question.points
+        else:
+            raise ValueError('Invalid question type')
+
+    return total_score
