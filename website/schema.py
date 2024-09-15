71de5795d40e62bc8d21a2d2818eda01817ad452
AlaaBadawii <alaaahmed16112000@gmail.com>
2024-09-15 23:24:55 +0300

    DOC: add documentation

--- website/schema.py
+++ website/schema.py
@@ -1,5 +1,7 @@
+""" This file contains the schema for the token data. """
 from pydantic import BaseModel
 
 class TokenData(BaseModel):
+    """ Token data schema. """
     user_id: int
     user_name: str
