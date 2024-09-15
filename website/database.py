a746937683b170bdd9de1a667eb3db4ce60288d7
AlaaBadawii <alaaahmed16112000@gmail.com>
2024-09-15 23:24:19 +0300

    DOC: add documentation

--- website/database.py
+++ website/database.py
@@ -1,3 +1,4 @@
+""" This file is used to create a database connection and session for the FastAPI application. """
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from config import settings
@@ -10,6 +11,10 @@ engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 def get_db():
+    """ Create a new database session.
+    Returns:
+        Session: A new database session.
+    """
     db = SessionLocal()
     try:
         return db
