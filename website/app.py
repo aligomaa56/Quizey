f88c069367fd55180ca92a3fa848e5ef31afda7f
AlaaBadawii <alaaahmed16112000@gmail.com>
2024-09-15 23:23:52 +0300

    DOC: add documentation

--- website/app.py
+++ website/app.py
@@ -1,14 +1,19 @@
+""" This module sets up and runs a Flask web application. """
 from flask import Flask
 from api.v1.routes import auth, views
 
 
 def create_app():
+    """
+    Creates a Flask application and registers the API routes.
+    """
     app = Flask(__name__)
         
     app.register_blueprint(auth, url_prefix="/api/v1/auth")
     app.register_blueprint(views, url_prefix="/api/v1/views")
 
     return app
+
 
 if __name__ == "__main__":
     app = create_app()
